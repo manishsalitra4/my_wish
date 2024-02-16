92d465b7f4637bb6d1261009aec717d652b8f1a0
manish <manishsalitra4@gmail.com>
2024-02-16 06:32:40 -0800

    'device_integrate'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_16_135643) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_16_141939) do
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "status"
@@ -40,10 +40,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_16_135643) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "wish_products", force: :cascade do |t|
